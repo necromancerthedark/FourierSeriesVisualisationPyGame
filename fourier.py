e5059f4384fb5a0887dd3078ea1ecc1bbdb220e5
Kumar Gaurav Pandey <kg22584@gmail.com>
2020-01-15 21:15:05 +0530

    Completed

--- fourier.py
+++ fourier.py
@@ -6,10 +6,14 @@ pygame.init()
 white = (255, 255, 255)
 black = (0, 0, 0)
 height = 800
+n = 1
 dots = []
-time = 0.0
+time = 0
 radius = 100
 gameloop = True
+clock = pygame.time.Clock()
+x = 0
+y = 0
 width = 600
 window = pygame.display.set_mode((height, width))
 
@@ -18,23 +22,37 @@ while gameloop:
         if events.type == pygame.QUIT:
             gameloop = False
 
-    x = radius * math.cos(time)
-    y = radius * math.sin(time)
+    x = y = 0
 
-    r = 0+random.randint(5, 250)
-    g = 0 + random.randint(5, 250)
-    b = 0+random.randint(5, 250)
-    dots.insert(0, y)
     window.fill(black)
-    pygame.draw.circle(window, white, (150, 300), radius, 1)
-    pygame.draw.circle(window, white, (150+int(x), 300+int(y)), 8)
-    pygame.draw.line(window, white, (150, 300), (150+int(x), 300+int(y)))
+    for i in range(30):
+        prevx = x
+        prevy = y
+        n = i*2 + 1
+        radius = 75 * (4/(n*math.pi))
+        x += radius * math.cos(n*time)
+        y += radius * math.sin(n*time)
+
+        r = 255  # 0+random.randint(5, 250)
+        g = 255  # 0 + random.randint(5, 250)
+        b = 255  # 0+random.randint(5, 250)
+
+        pygame.draw.circle(window, white, (150+int(prevx),
+                                           300+int(prevy)), int(radius), 1)
+        pygame.draw.circle(window, white, (150+int(x), 300+int(y)), 8)
+        pygame.draw.line(window, white, (150+int(prevx), 300+int(prevy)),
+                         (150+int(x), 300+int(y)))
+
+    clock.tick(300)
+
+    dots.insert(0, y)
     pygame.draw.line(window, (r, g, b), (150+x, 300 + y),
                      (350, 300+int(dots[0])))
-
     for i in range(len(dots)):
         gfxdraw.pixel(window, 350 + i, 300+int(dots[i]), (r, g, b))
+        # pygame.draw.polygon(window,white,((350+i,dots[i])))
     pygame.display.update()
     if len(dots) > 500:
         dots = dots[0:450]
+
     time += 0.01
